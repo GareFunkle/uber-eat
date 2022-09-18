448c3c7415c517573e2cb11ba1f9bcd5e39940c1
GarFunkle <vincent.durret@gmail.com>
2022-09-18 19:02:16 +0200

    add first components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomePage from './Page/HomePage.vue'
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    HomePage,
   }
 }
 </script>
-
+  
 <style>
+@import url('./style/reset.css');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
 }
 </style>
---
+++ src/Page/HomePage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <RestaurantRow v-for="(data, i) in data_restaurant" :three_restaurant="data" :key="i"  />
+  </div>
+</template>
+
+<script>
+import info from '../BD/BDD.js'
+import { onMounted, ref } from 'vue';
+
+import RestaurantRow from '../components/RestaurantRow.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    RestaurantRow,
+  },
+  setup() {
+    class Restaurant {
+      constructor(name, note, image, drive_time) {
+        this.name = name
+        this.note = note
+        this.image = image
+        this.drive_time = drive_time
+      }
+    }
+
+    let data_restaurant = ref([]);
+
+    const makeDataRestaurant = () => {
+      let three_restaurant = [];
+
+      for (const restaurant of info) {
+        const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time)
+
+        if (three_restaurant.length === 2) {
+          three_restaurant.push(new_restaurant);
+          data_restaurant.value.push(three_restaurant);
+          three_restaurant = [];
+        } else {
+          three_restaurant.push(new_restaurant);
+        }
+      }
+    };
+  
+    onMounted(makeDataRestaurant,);
+    
+    return {
+      data_restaurant,
+    }
+  },
+}
+</script>
+
+<style>
+</style>
